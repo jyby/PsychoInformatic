31c4f7b4585d2e26b5fb7b490d478b1c739fa21b
Jeremy 'LeJyBy' Barbay <jeremy@barbay.cl>
2015-05-12 08:02:46 -0400

    Added basic code for generating multiplication Flash Cards

--- LaRaNa/app/src/main/java/cl/lejyby/larana/Deck.java
+++ LaRaNa/app/src/main/java/cl/lejyby/larana/Deck.java
@@ -5,15 +5,41 @@ import java.util.Random;
  * Created by jbarbay on 11/05/15.
  */
 public class Deck {
-    int nbFlashCards;
     FlashCard[] flashCard;
     final Random rand = new Random();
+    final int x_min = 2;
+    final int x_max = 9;
+    final int y_min = 2;
+    final int y_max = 9;
+
+    public Deck() {
+        flashCard = new FlashCard[(x_max - x_min + 1) * (y_max - y_min + 1)];
+        for (int x = x_min; x <= x_max; x++) {
+            for (int y = y_min; y <= y_max; y++) {
+                flashCard[(x - x_min) * (y - y_min)] = new FlashCard(x, y);
+            }
+        }
+    }
+
 
     FlashCard pickAFlashCard() {
-        if(nbFlashCards>0) {
-            int index = rand.nextInt(nbFlashCards);
+        if (flashCard.length > 0) {
+            int index = rand.nextInt(flashCard.length);
             return flashCard[index];
         } else
             return null;
+    }
+
+    boolean match(String faceLeft, String faceRight) {
+        // Check if there is a line of the database containing face1 and face2
+        for (int i = 0; i < flashCard.length; i++) {
+            FlashCard card = flashCard[i];
+            String face0 = card.getFace(0);
+            String face1 = card.getFace(1);
+            if ((face1.matches(faceLeft) && face0.matches(faceRight)) ||
+                    (face0.matches(faceLeft) && face1.matches(faceRight)))
+                return true;
+        }
+        return false;
     }
 }
--- LaRaNa/app/src/main/java/cl/lejyby/larana/FlashCard.java
+++ LaRaNa/app/src/main/java/cl/lejyby/larana/FlashCard.java
@@ -1,9 +1,56 @@
 package cl.lejyby.larana;
+import java.util.Random;
 
 /**
  * Created by jbarbay on 11/05/15.
  */
 public class FlashCard {
-    int nb_faces;
-    String[] face;
+    private int id;
+    private String[] face;
+    public final Random rand = new Random();
+
+    public FlashCard() {
+        this.face = new String[2];
+        int x = rand.nextInt(10);
+        int y = rand.nextInt(10);
+        this.face[0] = Integer.toString(x)+"*"+Integer.toString(y);
+        this.face[1] = Integer.toString(x*y);
+    }
+    public FlashCard(int x, int y) {
+        this.face = new String[2];
+        this.face[0] = Integer.toString(x)+"*"+Integer.toString(y);
+        this.face[1] = Integer.toString(x*y);
+    }
+    public FlashCard(String faceA, String faceB) {
+        this.face = new String[2];
+        this.face[0] = faceA;
+        this.face[1] = faceB;
+    }
+    public FlashCard(int id, String faceA, String faceB) {
+        this.face = new String[2];
+        this.face[0] = faceA;
+        this.face[1] = faceB;
+        this.id = id;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public int getID() {
+        return this.id;
+    }
+
+    public void setFaces(String[] face) {
+        this.face = face;
+    }
+
+    public String[] getFaces() {
+        return this.face;
+    }
+
+    public String getFace(int i) {
+        return this.face[i];
+    }
+
 }
